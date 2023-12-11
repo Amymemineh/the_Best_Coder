7657281268a90c2cfce321bdc54e6ee37b7727dc
Amymemineh <152461105+Amymemineh@users.noreply.github.com>
2023-12-11 19:01:35 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,11 +1,4 @@
+answer = ["D", "C", "B", "A", "E"]
 for _ in range(3):
-    yut_list = list(map(int, input().split()))
-    if sum(yut_list) == 0:
-        print ("D")
-    elif sum(yut_list) == 1:
-        print ("C")
-    elif sum(yut_list) == 2:
-        print ("B")
-    elif sum(yut_list) == 3:
-        print ("A")
-    else: print("E")    
+    yut = sum(map(int, input().split()))
+    print(answer[yut])
