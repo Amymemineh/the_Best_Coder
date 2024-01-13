85a5475be2b601bc29a77af8d5131511ea16dd35
Amymemineh <eunhye.choung@gmail.com>
2024-01-13 20:44:28 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.py
@@ -0,0 +1,21 @@
+N, M, L = map(int, input().split())
+cnt = 0
+start = 0
+seat = []
+
+for i in range(N):
+  seat.append(0)
+
+seat[0] = 1
+
+while seat[start] < M:   
+  if seat[start]%2 != 0:
+    start = (start + L) % N
+    seat[start] += 1
+    cnt += 1    
+  else:
+    start = (start - L) % N
+    seat[start] += 1
+    cnt += 1
+
+print(cnt)                                                                                                                                                                       
