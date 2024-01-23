20f8ad2e75140b3b98f7253a01b8a1eb2fc35b55
Amymemineh <eunhye.choung@gmail.com>
2024-01-23 19:14:15 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,10 +1,10 @@
 a, b = map(int, input().split())
-max = (b//2)+1
 num = []
 
-for i in range(1, max+1):
+for i in range(1, b+1):
   for _ in range(i):
     num.append(i)
+    if len(num) >= b: break
 
 answer = sum(num[a-1:b])
 print(answer)
