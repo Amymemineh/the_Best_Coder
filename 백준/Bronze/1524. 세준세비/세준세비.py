6b29b5a0c767a0ec5fc8f2cf5fee80a1b380c352
Amymemineh <eunhye.choung@gmail.com>
2024-01-26 08:37:12 +0900

    [Bronze I] Title: 세준세비, Time: 220 ms, Memory: 41912 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -1,17 +1,22 @@
-t = int(input())
+T=int(input())
+for _ in range(T):
+  space=input() #공백 잡아 먹는 애
+  temp=[]
+  n,m=map(int,input().split())
+  #내림차순 정렬
+  jun=sorted(list(map(int,input().split())),reverse=False)
+  bi=sorted(list(map(int,input().split())),reverse=False)
 
-for i in range(t):
-  input()
-  n, m = map(int, input().split())
-  jun = sorted([int(j) for j in input().split()])
-  bi = sorted([int(b) for b in input().split()])
-  
+  #pop은 뒤에서부터 빠짐
   while jun and bi:
-    if jun[0] < bi[0]:
-      jun.pop(0)
+    if jun[-1]>=bi[-1]:
+      bi.pop()
     else:
-      bi.pop(0)
-  
-  if jun: print("S")
-  elif bi: print("B")
-  else: print("C")
+      jun.pop()
+
+  if jun:
+    print('S')
+  elif bi:
+    print('B')
+  else:
+    print('C')
