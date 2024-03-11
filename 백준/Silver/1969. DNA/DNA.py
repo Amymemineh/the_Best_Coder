0e4a4bdf2cee3575e45eb3369e7d012839eceea5
Amymemineh <eunhye.choung@gmail.com>
2024-03-11 14:34:00 +0900

    [Silver IV] Title: DNA, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+dna = [input() for _ in range(n)]
+result = []
+ham_distance = 0
+
+
+for i in range(m):
+  dna_dic = {"A":0, "T":0, "G":0, "C":0}  
+  for j in range(n):
+    dna_dic[dna[j][i]] += 1
+    
+  ham = max(dna_dic.values())
+  answer = sorted([key for key, value in dna_dic.items() if value == ham])
+  result.append(answer[0])
+  ham_distance += n - ham 
+
+print(''.join(result), ham_distance, sep = '\n')
+    
