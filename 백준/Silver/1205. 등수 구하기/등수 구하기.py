9209551d531b6709d144173a307a4c7451b766ec
Amymemineh <eunhye.choung@gmail.com>
2024-03-12 16:54:08 +0900

    [Silver IV] Title: 등수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -1,10 +1,8 @@
 n, new, p = map(int, input().split())
-score_list = []
-if n > 0: score_list = list(map(int, input().split()))
+score_list = list(map(int, input().split())) if n > 0 else []
 
 if len(score_list) >= p and new <= score_list[-1]:
   print(-1)
 else:
-  score_list.append(new)
-  score_list.sort(reverse = True)
+  score_list = sorted(score_list + [new], reverse = True)
   print(score_list.index(new) + 1)
