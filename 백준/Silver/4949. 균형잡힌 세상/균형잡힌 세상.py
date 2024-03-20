9e2352ccc993b5cca691ee2cce54a5e1c26b92d0
Amymemineh <eunhye.choung@gmail.com>
2024-03-20 13:49:14 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,24 @@
+# import sys
+# input = sys.stdin.read
+
+while True:
+  text = input()
+  if text == ".": break
+
+  stack = []
+  
+  for i in text:
+    if i == "(" or i == "[": stack.append(i) 
+    elif i == ")":
+      if len(stack) != 0 and stack[-1] == "(": stack.pop()
+      else: 
+        stack.append(i)
+        break
+    elif i == "]":
+      if len(stack) != 0 and stack[-1] == "[": stack.pop()
+      else: 
+        stack.append(i)
+        break
+        
+  if stack: print("no")
+  else: print("yes")
