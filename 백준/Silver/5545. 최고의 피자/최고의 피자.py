4de7d3e1ddffa587fc16b12179de43c18bbe2da8
Amymemineh <eunhye.choung@gmail.com>
2024-03-25 21:43:42 +0900

    [Silver III] Title: 최고의 피자, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5545. 최고의 피자/최고의 피자.py
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -3,14 +3,12 @@ a, b = map(int, input().split())
 dough = int(input())
 pizza = dough
 best = dough / a
+topping = sorted([int(input()) for _ in range(n)])
 
-if n != 0: 
-  topping = sorted([int(input()) for _ in range(n)])
-
-  for i in range(1, n+1):
-    pizza += topping[-1]
-    if best < pizza / (a + b * i):
-      best = pizza / (a + b * i)
-      topping.pop()
+for i in range(1, n+1):
+  pizza += topping[-1]
+  if best < pizza / (a + b * i):
+    best = pizza / (a + b * i)
+    topping.pop()
 
 print(int(best))
