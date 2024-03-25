5ca5f8c374c5216552759493549afeb598d81839
Amymemineh <eunhye.choung@gmail.com>
2024-03-25 13:54:25 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
@@ -1,19 +1,12 @@
 n, m = map(int, input().split())
 journal = set(map(int, input().split()))
-paper = []
-
-for i in range(1, n+1):
-  if i not in journal:
-    paper.append(i)
 
 page = 0
 ink = 0
 
-for i in paper:
-  if page:
-    ink += min(7, (i - page) * 2)
-  else:
-    ink += 7
-  page = i
+for i in range(1, n + 1):
+    if i not in journal:
+        ink += 7 if not page else min(7, (i - page) * 2)
+        page = i
 
 print(ink)
