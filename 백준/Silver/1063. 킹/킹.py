37ad929059749aead53d42ffc4bf5373eb09932d
Amymemineh <eunhye.choung@gmail.com>
2024-03-27 21:39:35 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -3,7 +3,6 @@ dic = {"R":[1, 0], "L":[-1, 0], "B":[0, -1], "T":[0, 1], "RT":[1, 1], "LT":[-1, 
 king, stone, n = input().split()
 king_x, king_y = ord(king[0]), int(king[1])
 stone_x, stone_y = ord(stone[0]), int(stone[1])
-initial_kx, initial_ky = king_x, king_y
 n = int(n)
 
 move = [input() for _ in range(n)]
@@ -11,12 +10,12 @@ move = [input() for _ in range(n)]
 for m in move:
   dx, dy = dic[m][0], dic[m][1]
   new_kx, new_ky = king_x + dx, king_y + dy
-  if new_kx < ord('A') or new_kx > ord('H') or new_ky < 1 or new_ky > 8:
-    continue
+    
+  if not(ord('A') <= new_kx <= ord('H') and 1 <= new_ky <= 8): continue
+    
   if new_kx == stone_x and new_ky == stone_y:
     new_sx, new_sy = stone_x + dx, stone_y + dy
-    if new_sx < ord('A') or new_sx > ord('H') or new_sy < 1 or new_sy > 8:
-      continue
+    if not(ord('A') <= new_sx <= ord('H') and 1 <= new_sy <= 8): continue
     stone_x, stone_y = new_sx, new_sy
   king_x, king_y = new_kx, new_ky
 
