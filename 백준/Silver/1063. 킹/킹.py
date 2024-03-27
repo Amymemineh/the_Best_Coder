fa51d65c3153fc2fe3356cc04247cfe974fcfc00
Amymemineh <eunhye.choung@gmail.com>
2024-03-27 21:42:59 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,4 +1,5 @@
-dic = {"R":[1, 0], "L":[-1, 0], "B":[0, -1], "T":[0, 1], "RT":[1, 1], "LT":[-1, 1], "RB":[1, -1], "LB":[-1, -1]}
+dic = {"R":[1, 0], "L":[-1, 0], "B":[0, -1], "T":[0, 1], 
+       "RT":[1, 1], "LT":[-1, 1], "RB":[1, -1], "LB":[-1, -1]}
 
 king, stone, n = input().split()
 king_x, king_y = ord(king[0]), int(king[1])
@@ -10,9 +11,7 @@ move = [input() for _ in range(n)]
 for m in move:
   dx, dy = dic[m][0], dic[m][1]
   new_kx, new_ky = king_x + dx, king_y + dy
-    
   if not(ord('A') <= new_kx <= ord('H') and 1 <= new_ky <= 8): continue
-    
   if new_kx == stone_x and new_ky == stone_y:
     new_sx, new_sy = stone_x + dx, stone_y + dy
     if not(ord('A') <= new_sx <= ord('H') and 1 <= new_sy <= 8): continue
